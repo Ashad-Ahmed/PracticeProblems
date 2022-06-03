fa3c1319e97c12045b19da231fc8e41ee2e35709
Ashad Ahmed <72685188+Ashad-Ahmed@users.noreply.github.com>
2022-06-03 06:24:40 +0530

    Update Java.java

--- Java.java
+++ Java.java
@@ -1,4 +1,4 @@
-// Problem: Check if the nodes of a given BST lie within a given range of Min and Max
+// Problem 1: Check if the nodes of a given BST lie within a given range of Min and Max
 // A problem to traverse the entire BST and check if the nodes satisfy a given condition.
 // Key: While traversing the tree, replace the "Print" statement with a logic to check for the given condition.
 // Variants: return true/false, return number of nodes satisfying/not satisfying above condition.
@@ -67,3 +67,101 @@ class MyArray{
     System.out.print(tree.isRange(20, 44));
     }
 }
+
+
+//Problem 2: Find a pair that sums up to a given target.
+// Notice the problem solving approach, this approach can be extended to problems where we find subarray that sums up to a value. (Problem3)
+
+import java.util.HashMap;
+import java.util.Map;
+
+class MyArray
+{
+    // Function to find a pair in an array with a given sum using hashing
+    public static void findPair(int[] nums, int target)
+    {
+        // create an empty HashMap
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // do for each element
+        for (int i = 0; i < nums.length; i++)
+        {
+            // check if pair (nums[i], target-nums[i]) exists
+
+            // if the difference is seen before, print the pair
+            if (map.containsKey(target - nums[i]))
+            {
+                System.out.printf("Pair found (%d, %d)",
+                        nums[map.get(target - nums[i])], nums[i]);
+                return;
+            }
+
+            // store index of the current element in the map
+            map.put(nums[i], i);
+        }
+
+        // we reach here if the pair is not found
+        System.out.println("Pair not found");
+    }
+
+    public static void main (String[] args)
+    {
+        int[] nums = { 8, 7, 2, 5, 3, 1 };
+        int target = 10;
+
+        findPair(nums, target);
+    }
+}
+
+//Problem 2: Find a subarray that sums up to a target value. (Say 0, 9 etc..)
+
+import java.util.HashMap;
+import java.util.Map;
+
+class MyArray
+{
+    // Function to print subarray having a given sum using hashing
+    public static void findSubarray(int[] nums, int target)
+    {
+        // create an empty map
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // insert (0, -1) pair into the set to handle the case when a
+        // subarray with the given sum starts from index 0
+        map.put(0, -1);
+
+        // keep track of the sum of elements so far
+        int sum_so_far = 0;
+
+        // traverse the given array
+        for (int i = 0; i < nums.length; i++)
+        {
+            // update `sum_so_far`
+            sum_so_far += nums[i];
+
+            // if `sum_so_far - target` is seen before, we have found
+            // the subarray with sum equal to `target`
+            if (map.containsKey(sum_so_far - target))
+            {
+                System.out.println("Subarray found [" +
+                                (map.get(sum_so_far - target) + 1) +
+                                "–" + i + "]");
+                return;
+            }
+
+            // insert (current sum, current index) pair into the map
+            map.put(sum_so_far, i);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        // an integer array
+        int[] nums = { 5, -7, 3, -4, 8, 2, 6, 1, 4, 1, 10 };
+        int target = 9;
+
+        findSubarray(nums, target);
+    }
+}
+
+
