7236830540949530ca458d82117685f3ea3b842d
Ashad Ahmed <72685188+Ashad-Ahmed@users.noreply.github.com>
2022-07-18 11:26:03 +0530

    Update Concepts.java

--- Concepts.java
+++ Concepts.java
@@ -21,3 +21,19 @@ class MyArray{
         System.out.println(rec(3, str, "", ""));
     }
 }
+
+// Below is the tabulation code for Fibonacci : keeping arr[0] = 1, arr[1] = 1 it can solve climbing stair problem. Keeping arr[0] = 1, arr[1] = 2 can solve Number of ways house can be placed problem
+// arr[i-1] + arr[i-2] means we've two options, either consume one input at once or consume two inputs at once. Consuming inputs might mean "Selecting". 
+
+
+class Solution {
+    public int fib(int n) {
+        int[] arr=new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2; i <= n; i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+    }
+}
